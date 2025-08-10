18ccf6e5991c3b30f55df33f075e9680004c89fe
tirulesnar07 <tirulesnar@gmail.com>
2025-08-10 23:18:06 +0530

    adding again

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
 
-<<<<<<< HEAD
-print('hello World')
-
-a="Tiru"
-age="22"
-country="India"
-print(a,age,country)
-
-# changing to tesing 
-=======
 
 
 # Hey man how are 
->>>>>>> 69437e9 (modified for testing)
