1cc787d0b480f44204edd5c865234a77859a2f2c
tirulesnar07 <tirulesnar@gmail.com>
2025-08-11 23:46:51 +0530

    adding new files

---
+++ keywords.py
@@ -0,0 +1,8 @@
+
+#Listing all keywords!!
+import keyword
+print(keyword.kwlist)\
+
+# Don't assign keywords as variables
+class=10
+print(class)
---
+++ variables.py
@@ -0,0 +1,28 @@
+# Assign the values:
+
+
+#Getting datatype of variables
+
+x=10
+y="Hi Robo"
+z=4.5
+
+li = [1, 2, 3]
+d = {'key': 'value'}
+bool = True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(d))
+print(type(bool))
+print(type(li))
+
+#Assignin same value 
+p=q=r=100
+print(x,y,z)
+
+#Assiging different datatypes without mentioning!
+
+a,b,c=11.11,324,"Hi 420"
+print(a,b,c)
